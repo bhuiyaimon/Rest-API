8f1b649f69afffc457069fc96423e181d2179d76
imonbhuiya2 <imonbhuiya2@gmail.com>
2025-01-08 23:25:48 +0100

    change commit

--- src/main/java/com/restapi/SpringbootRestApiApplication.java
+++ src/main/java/com/restapi/SpringbootRestApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootRestApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootRestApiApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
