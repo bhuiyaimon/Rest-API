743e1c46f47ba2f7f91e11140a78efd9539c4939
imonbhuiya2 <imonbhuiya2@gmail.com>
2025-01-09 00:03:13 +0100

    Create Spring Boot REST API That Returns List as JSON

--- src/main/java/com/restapi/controller/StudentController.java
+++ src/main/java/com/restapi/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.restapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController
 // http://localhost:8080/student
@@ -18,6 +21,18 @@ public class StudentController
 
        );
        return student;
+    }
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"Imon","Bhuiya"));
+        students.add(new Student(2,"Imon1","Bhuiya"));
+        students.add(new Student(3,"Imon2","Bhuiya"));
+        students.add(new Student(3,"Imon3","Bhuiya"));
+        return students;
+
+
     }
 
 
